da9f61fed69d8374bd989004bc53878e25408fe6
Debashis Jena <debashisjena100@gmail.com>
2025-11-18 14:58:30 +0530

    fix: update import path for api in auth and user actions

--- frontend/src/redux/actions/authActions.js
+++ frontend/src/redux/actions/authActions.js
@@ -1,4 +1,4 @@
-import api from 'utils/api';
+import api from "../../utils/api";
 
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
 export const LOGIN_FAIL = 'LOGIN_FAIL';
--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import api from 'utils/api';
+import api from "../../utils/api";
 
 export const GET_USERS_SUCCESS = 'GET_USERS_SUCCESS';
 export const GET_USERS_FAIL = 'GET_USERS_FAIL';
