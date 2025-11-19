f08ff45bae5ad1dead451f30d05ca8f358f28648
debashisjena100 <debashisjena100@gmail.com>
2025-11-19 00:12:54 +0000

    css issue fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
+import './index.css';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
